c8dc3e1c65e65ceddfa9324d2efd566cb4afba18
nullgemm <nullgemm@mailbox.org>
2021-03-20 13:47:05 +0100

    fix remotes script

--- make/scripts/git_remotes.sh
+++ make/scripts/git_remotes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git remote add gitea ssh://git@git.nullgemm.fr:2999/nullgemm/razorbeard.git
-git remote add github git@github.com:nullgemm/razorbeard.git
+git remote add gitea ssh://git@git.nullgemm.fr:2999/nullgemm/desktopsample.git
+git remote add github git@github.com:nullgemm/desktopsample.git
 git remote remove origin
